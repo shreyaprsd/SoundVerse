5f6822d6eb097b5ea09f52af5ffc300a823f30c0
Shreya Prasad <prasadshreya2303@gmail.com>
2026-05-07 21:22:09 +0530

    Initial Commit

---
+++ SoundVerse/SoundVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundVerseApp.swift
+//  SoundVerse
+//
+//  Created by Shreya Prasad on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
